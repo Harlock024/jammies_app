3abb8d41f8fa50f93e31e1ee5de98b56a116ddec
harlock024 <hadith024@gmail.com>
2025-04-26 03:08:51 -0600

    feat: audio state added

--- lib/mocks/mock_track.dart
+++ lib/mocks/mock_track.dart
@@ -3,32 +3,91 @@ import '../models/track.dart';
 final List<Track> mockTracks = [
   Track(
     id: '1',
-    title: 'Test Track',
-    artist: 'Test Artist',
-    duration: '3:20',
+    title: 'Presage',
+    artist: 'Color out',
+    duration: '04:09',
     coverUrl:
-        'https://i.scdn.co/image/ab67616d0000b27393c50048dce0f88071728c8c',
-    audioUrl: '../music/Consume.mp3',
+        'https://i.pinimg.com/236x/2d/ad/94/2dad9400bf8c395a87f36b9932814c48.jpg',
+    audioUrl:
+        'https://res.cloudinary.com/drdefvojb/video/upload/v1745655631/jammies_track/zpq8xlhfndjouvcecwxc.mp3',
     album: "Test Album",
   ),
+
   Track(
     id: '2',
-    title: 'Test Track 2',
-    artist: 'Test Artist 2',
+    title: 'Summer',
+    artist: 'Rusty Tea Markes',
     duration: '4:20',
     coverUrl:
-        'https://i.scdn.co/image/ab67616d0000b27393c50048dce0f88071728c8c',
-    audioUrl: '../music/Consume.mp3',
+        'https://i.pinimg.com/236x/76/38/16/763816c35030cff89522096c22531b77.jpg',
+    audioUrl:
+        'https://res.cloudinary.com/drdefvojb/video/upload/v1745655630/jammies_track/jdl99aaswnw4aoafinr7.mp3',
     album: "Test Album 2",
   ),
   Track(
     id: '3',
-    title: 'Test Track 3',
-    artist: 'Test Artist 3',
+    title: 'Grand Daddy',
+    artist: 'Victoria Cafe',
     duration: '5:20',
     coverUrl:
-        'https://i.scdn.co/image/ab67616d0000b27393c50048dce0f88071728c8c',
-    audioUrl: '../music/Consume.mp3',
+        'https://i.pinimg.com/236x/23/ca/05/23ca054c4b310fdb3f3435b8c9884d33.jpg',
+    audioUrl:
+        'https://res.cloudinary.com/drdefvojb/video/upload/v1745655630/jammies_track/ahlxulzzs1hm35dclioa.mp3',
     album: "Test Album 3",
+  ),
+  Track(
+    id: '4',
+    title: 'Until Tonight',
+    artist: 'Back on earth',
+    duration: '4:20',
+    coverUrl:
+        'https://i.pinimg.com/236x/6d/62/be/6d62bebabcab4e7e8fe50dc462e8bfab.jpg',
+    audioUrl:
+        'https://res.cloudinary.com/drdefvojb/video/upload/v1745655630/jammies_track/echvgxdnpoazqteq8mcq.mp3',
+    album: "Test Album 4",
+  ),
+  Track(
+    id: '5',
+    title: 'Broken Love',
+    artist: 'Yigit Atilla',
+    duration: '4:20',
+    coverUrl:
+        'https://i.pinimg.com/236x/5e/e7/3a/5ee73aed06e2e9d0658640650652f516.jpg',
+    audioUrl:
+        'https://res.cloudinary.com/drdefvojb/video/upload/v1745655628/jammies_track/d3ydggg7fjpm07mq8eoe.mp3',
+    album: "Test Album 5",
+  ),
+  Track(
+    id: '6',
+    title: 'Rising Bones',
+    artist: 'Mr. Rock',
+    duration: '4:20',
+    coverUrl:
+        'https://i.pinimg.com/474x/ae/4c/9f/ae4c9f4b7f64e967d3192968c07490a3.jpg',
+    audioUrl:
+        'https://res.cloudinary.com/drdefvojb/video/upload/v1745655626/jammies_track/nqquzt56ruqtcv2vljdn.mp3',
+    album: "Test Album 5",
+  ),
+  Track(
+    id: '7',
+    title: 'Molotov Heart',
+    artist: 'Mr. Rock',
+    duration: '4:20',
+    coverUrl:
+        'https://i.pinimg.com/236x/e5/3a/09/e53a09742855ded6cd7be970415b5560.jpg',
+    audioUrl:
+        'https://res.cloudinary.com/drdefvojb/video/upload/v1745655625/jammies_track/i2v7f6xkfkyl0znhbzlc.mp3',
+    album: "Test Album 5",
+  ),
+  Track(
+    id: '8',
+    title: 'Everybody',
+    artist: 'Kara',
+    duration: '4:20',
+    coverUrl:
+        'https://i.pinimg.com/236x/a8/85/d2/a885d205fd265e97e5db249bb8c75104.jpg',
+    audioUrl:
+        'https://res.cloudinary.com/drdefvojb/video/upload/v1745655623/jammies_track/sdmbmjwlskaidlrdgtsi.mp3',
+    album: "Test Album 5",
   ),
 ];
--- lib/widgets/tracks/track_card.dart
+++ lib/widgets/tracks/track_card.dart
@@ -1,74 +1,65 @@
 import 'package:flutter/material.dart';
 import 'package:jammies_app/models/track.dart';
+import 'package:jammies_app/providers/audio_player.dart';
+import 'package:provider/provider.dart';
 
 class TrackCard extends StatelessWidget {
   final Track track;
+  final VoidCallback? onTap;
+  final VoidCallback? onPlay;
 
-  const TrackCard({super.key, required this.track});
+  const TrackCard({super.key, required this.track, this.onTap, this.onPlay});
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      margin: EdgeInsets.all(8),
-      elevation: 5, // Da un efecto de sombra para resaltar la tarjeta
-      shape: RoundedRectangleBorder(
-        borderRadius: BorderRadius.circular(12), // Bordes redondeados
-      ),
-      child: Padding(
-        padding: EdgeInsets.all(16),
-        child: Row(
-          // Usamos Row para la imagen y el contenido de forma horizontal
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            // Imagen de la portada del track
-            ClipRRect(
-              borderRadius: BorderRadius.circular(8),
-              child: Image.network(
-                track.coverUrl,
-                width: 80,
-                height: 80,
-                fit: BoxFit.cover,
+    return InkWell(
+      onTap: () {
+        context.read<AudioController>().setTrack(track);
+      },
+      child: Card(
+        margin: EdgeInsets.all(8),
+        elevation: 5,
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(12)),
+        child: Padding(
+          padding: EdgeInsets.all(16),
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.circular(8),
+                child: Image.network(
+                  track.coverUrl,
+                  width: 80,
+                  height: 80,
+                  fit: BoxFit.cover,
+                ),
               ),
-            ),
-
-            SizedBox(width: 16), // Separación entre imagen y texto
-            Expanded(
-              // Hace que el texto ocupe el resto del espacio disponible
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  // Título del track
-                  Text(
-                    track.title,
-                    style: Theme.of(context).textTheme.bodyMedium,
-                    overflow:
-                        TextOverflow.ellipsis, // Evita que se desborde el texto
-                  ),
-                  SizedBox(height: 4),
-
-                  // Artista del track
-                  Text(
-                    track.artist,
-                    style: Theme.of(context).textTheme.bodySmall,
-                    overflow: TextOverflow.ellipsis,
-                  ),
-                  SizedBox(height: 4),
-
-                  // Duración del track
-                  Text(
-                    _formatDuration(track.duration),
-                    style: Theme.of(context).textTheme.bodySmall,
-                  ),
-                ],
+              SizedBox(width: 16),
+              Expanded(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      track.title,
+                      style: Theme.of(context).textTheme.bodyMedium,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      track.artist,
+                      style: Theme.of(context).textTheme.bodySmall,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    SizedBox(height: 4),
+                    Text(
+                      _formatDuration(track.duration),
+                      style: Theme.of(context).textTheme.bodySmall,
+                    ),
+                  ],
+                ),
               ),
-            ),
-            IconButton(
-              icon: Icon(Icons.play_arrow, size: 32),
-              onPressed: () {
-                // Implement play functionality here
-              },
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
